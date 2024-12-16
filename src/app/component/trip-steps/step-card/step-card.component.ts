a735ea25a5c443ff5f30d6797a6cad7b42996b5c
zeinab-alhwary <zeinabnebeshi20@gmail.com>
2024-12-16 14:28:48 +0200

    adding some style condition and fixed responsive style

--- src/app/component/trip-steps/step-card/step-card.component.ts
+++ src/app/component/trip-steps/step-card/step-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-step-card',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./step-card.component.scss']
 })
 export class StepCardComponent {
-
+ @Input() public contentCards:any;
 }
