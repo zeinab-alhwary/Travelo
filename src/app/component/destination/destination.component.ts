32f3873ea901b710f36f82cb5c3bf89660710f53
zeinab-alhwary <zeinabnebeshi20@gmail.com>
2024-12-23 13:43:46 +0200

    change image path

--- src/app/component/destination/destination.component.ts
+++ src/app/component/destination/destination.component.ts
@@ -7,22 +7,22 @@ import { Component } from '@angular/core';
 })
 export class DestinationComponent {
   rateCards = [{
-    image: '../../../assets/images/italy-image.png',
+    image: '/Travelo/assets/images/italy-image.png',
     head: 'Rome, Italy',
     description: 'Mixing ancient Roman ruins, renaissance art and contemporary Italian cuisine, Rome is an unmissable destination.',
   },
   {
-    image: '../../../assets/images/france.png',
+    image: '/Travelo/assets/images/france.png',
     head: 'Paris, France',
     description: 'France’s capital, is a major European city and a global center for art & culture.'
   },
   {
-    image: '../../../assets/images/greece.png',
+    image: '/Travelo/assets/images/greece.png',
     head: 'Santorini, Greece',
     description: 'one of the most famous islands in the world. remarkable beauty of an active volcano is priceless!'
   },
   {
-    image: '../../../assets/images/thailand.png',
+    image: '/Travelo/assets/images/thailand.png',
     head: 'Thailand',
     description: 'known for its beautiful beaches, temples, and food. and its people are known for their warm hospitality.'
   }
